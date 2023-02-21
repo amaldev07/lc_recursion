1d17cc5c51e143f7ada31df110845fa48d4e15d1
Amaldev <88559916+amaldev07@users.noreply.github.com>
2023-02-21 21:28:53 +0530

    Create 1155.ts

---
+++ 1155.ts
@@ -0,0 +1,20 @@
+function numRollsToTarget(n, k, target) {
+    let count = recFaces('', target, k, n, 0);
+    return count;
+};
+
+function recFaces(p, up, faces, n, count) {
+    if (up == 0) {
+        return count+1;
+    }
+    let ret = [];
+    for (let i = 0; i < (up + 1) && i < faces; i++) {
+        let newP = p + (i + 1)
+        if ((up - (i + 1)) == 0 && newP.length <= n) {
+            count = count + (recFaces(newP, up - (i + 1), faces, n,0));
+        } else if (newP.length < n) {
+            count = count + (recFaces(newP, up - (i + 1), faces, n,0));
+        }
+    }
+    return count;
+}
